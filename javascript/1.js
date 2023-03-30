0d58219b55f991f7130922cf558bb13e6ded1a7d
sunilsuraci <suracisunil@gmail.com>
2023-03-30 14:19:48 +0200

    final js

--- javascript/1.js
+++ javascript/1.js
@@ -81,3 +81,47 @@ window.HUB_EVENTS = {
         }
         return !1
       },
+      _sendErrorMessageToEditor: function () {
+        try {
+          if (this._shouldPostMessage()) {
+            var E = {
+              topic: HUB_EVENTS.PEN_ERROR_INFINITE_LOOP,
+              data: {
+                line: this._findAroundLineNumber()
+              }
+            };
+            parent.postMessage(E, "*")
+          } else this._throwAnErrorToStopPen()
+        } catch (_) {
+          this._throwAnErrorToStopPen()
+        }
+      },
+      _shouldPostMessage: function () {
+        return document.location.href.match(/boomboom/)
+      },
+      _throwAnErrorToStopPen: function () {
+        throw "We found an infinite loop in your Pen. We've stopped the Pen from running. More details and workarounds at https://blog.codepen.io/2016/06/08/can-adjust-infinite-loop-protection-timing/"
+      },
+      _findAroundLineNumber: function () {
+        var E = new Error,
+          _ = 0;
+        if (E.stack) {
+          var o = E.stack.match(/boomboom\S+:(\d+):\d+/);
+          o && (_ = o[1])
+        }
+        return _
+      },
+      _checkOnInfiniteLoop: function (E, _) {
+        if (!this._loopTimers[E]) return this._loopTimers[E] = _, !1;
+        var o;
+        if (_ - this._loopTimers[E] > this.MAX_TIME_IN_LOOP_WO_EXIT) throw "Infinite Loop found on loop: " + E
+      },
+      _getTime: function () {
+        return +new Date
+      }
+    }, window.CP.shouldStopExecution = function (E) {
+      var _ = window.CP.PenTimer.shouldStopLoop(E);
+      return !0 === _ && console.warn("[CodePen]: An infinite loop (or a loop taking too long) was detected, so we stopped its execution. More details at https://blog.codepen.io/2016/06/08/can-adjust-infinite-loop-protection-timing/"), _
+    }, window.CP.exitedLoop = function (E) {
+      window.CP.PenTimer.exitedLoop(E)
+    };
